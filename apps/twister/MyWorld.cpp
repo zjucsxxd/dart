54c81744a3aadd20a715aaa5971ba54aa406907e
Karen Liu <ckarenliu@gmail.com>
2014-04-07 08:27:41 -0400

    added some comments

--- apps/twister/MyWorld.cpp
+++ apps/twister/MyWorld.cpp
@@ -32,35 +32,36 @@ void MyWorld::solve() {
     for (int i = 0; i < numIter; i++) {
         gradients = updateGradients();
         newPose = mSkel->getConfig() - alpha * gradients;
-        mSkel->setConfig(newPose);
+        mSkel->setConfig(newPose); // DART updates all the transformations based on newPose
     }
 }
 
-// Current code only works for the left ankle
+// Current code only works for the left ankle with only one constraint
 VectorXd MyWorld::updateGradients() {
     // compute c(q)
     mC = mSkel->getMarker(mConstrainedMarker)->getWorldCoords() - mTarget;
 
     // compute J(q)
     Vector4d offset;
-    offset << mSkel->getMarker(mConstrainedMarker)->getLocalCoords(), 1;
+    offset << mSkel->getMarker(mConstrainedMarker)->getLocalCoords(), 1; // Create a vector in homogeneous coordinates
     // w.r.t ankle
     BodyNode *node = mSkel->getMarker(mConstrainedMarker)->getBodyNode();
     Joint *joint = node->getParentJoint();
     Matrix4d worldToParent = node->getParentBodyNode()->getWorldTransform().matrix();
     Matrix4d parentToJoint = joint->getTransformFromParentBodyNode().matrix();
-    Matrix4d dR = joint->getTransformDerivative(0);
+    Matrix4d dR = joint->getTransformDerivative(0); // Doesn't need .matrix() because it returns a Matrix4d instead of Isometry3d
     Matrix4d R = joint->getTransform(1).matrix();
     Matrix4d jointToChild =joint->getTransformFromChildBodyNode().inverse().matrix();
-    Vector4d J = worldToParent * parentToJoint * dR * R * jointToChild * offset;
+    Vector4d jCol = worldToParent * parentToJoint * dR * R * jointToChild * offset;
     int colIndex = joint->getGenCoord(0)->getSkeletonIndex();
-    mJ.col(colIndex) = J.head(3);
+    mJ.col(colIndex) = jCol.head(3); // Take the first 3 elelemtns of J
     dR = joint->getTransformDerivative(1);
     R = joint->getTransform(0).matrix();
-    J = worldToParent * parentToJoint * R * dR * jointToChild * offset;
+    jCol = worldToParent * parentToJoint * R * dR * jointToChild * offset;
     colIndex = joint->getGenCoord(1)->getSkeletonIndex();
-    mJ.col(colIndex) = J.head(3);
+    mJ.col(colIndex) = jCol.head(3);
     
+    // compute gradients
     VectorXd gradients = 2 * mJ.transpose() * mC;
     return gradients;
 }
